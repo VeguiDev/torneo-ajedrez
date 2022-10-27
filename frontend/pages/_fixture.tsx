2cf81c327d13288b88a8f1c94f86523b18eb6da6
= <=>
2022-10-27 00:22:09 -0300

    Graph Test

--- frontend/pages/_fixture.tsx
+++ frontend/pages/_fixture.tsx
@@ -1,51 +1,61 @@
-import styles from '../styles/Fixture.module.css';
+import styles from "../styles/Fixture.module.css";
 
-export function isOdd(n:number) {return n % 2;}
+export function isOdd(n: number) {
+  return n % 2;
+}
 
 export function Fixture() {
-
-    let children = [];
-
-    let players = 8;
-
-    for(let i = 0; i < players; i++) {
-        if(isOdd(i+1)) {
-            children.push(<FixtureRound players={2}></FixtureRound>)
-        }
-    }
-
-    return (
-        <div className={styles["fixture"]}>
-            {children}
-        </div>
-    );
-
+  return (
+    <div className={styles["fixture-wrapper"]}>
+      <object data="/_graph.svg" type="image/svg+xml"></object>
+    </div>
+  );
 }
 
-export function FixtureRound(props:{
-    players:number
-}) {
+// export function FixtureTree(props:{
+//     players:number
+// }) {
 
-    let children = [];
+//     let children = [];
 
-    for(let i = 0; i < props.players; i++) {
-        children.push(<FixturePlayer></FixturePlayer>)
-    }
+//     for(let i = 0; i > props.players; i++) {
+//         if(isOdd(i+1)) {
 
-    return (
-        <div className={styles["round"]}>
-            {children}
-        </div>
-    );
+//             children.push(<FixtureRound players={2}></FixtureRound>);
 
-}
+//         }
+//     }
 
-export function FixturePlayer() {
+//     return (
+//         <>{children}</>
+//     );
 
-    return (
-        <div className={styles["player"]}>
-            
-        </div>
-    );
+// }
 
-}
+// export function FixtureRound(props:{
+//     players:number
+// }) {
+
+//     let children = [];
+
+//     for(let i = 0; i < props.players; i++) {
+//         children.push(<FixturePlayer></FixturePlayer>)
+//     }
+
+//     return (
+//         <div className={styles["round"]}>
+//             {children}
+//         </div>
+//     );
+
+// }
+
+// export function FixturePlayer() {
+
+//     return (
+//         <div className={styles["player"]}>
+
+//         </div>
+//     );
+
+// }
